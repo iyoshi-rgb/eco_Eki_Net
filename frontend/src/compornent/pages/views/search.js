b14684fc3db2e5db54a20a7c04325a7a73a2fb68
Yuka <yukikazu.610@gmail.com>
2024-02-06 18:46:27 +0900

    駅検索画面コード書いた

--- frontend/src/compornent/pages/views/search.js
+++ frontend/src/compornent/pages/views/search.js
@@ -2,6 +2,60 @@ function Search(){
     return(
         <div>
             <p>駅検索</p>
+            <div style={{width: '100%', height: '100%', position: 'relative', background: 'white'}}>
+    <div style={{width: 430, height: 86, left: 0, top: 846, position: 'absolute', background: '#5EE030'}}>
+        <div style={{width: 86, height: 0, left: 169, top: 0, position: 'absolute', transform: 'rotate(90deg)', transformOrigin: '0 0', border: '3px white solid'}}></div>
+        <div style={{width: 86, height: 0, left: 255, top: 0, position: 'absolute', transform: 'rotate(90deg)', transformOrigin: '0 0', border: '3px white solid'}}></div>
+        <div style={{width: 86, height: 0, left: 342, top: 0, position: 'absolute', transform: 'rotate(90deg)', transformOrigin: '0 0', border: '3px white solid'}}></div>
+        <img style={{width: 63, height: 63, left: 10, top: 18, position: 'absolute'}} src="https://via.placeholder.com/63x63" />
+        <img style={{width: 63, height: 63, left: 269, top: 20, position: 'absolute'}} src="https://via.placeholder.com/63x63" />
+        <img style={{width: 60, height: 60, left: 183, top: 19, position: 'absolute'}} src="https://via.placeholder.com/60x60" />
+        <img style={{width: 60, height: 60, left: 99, top: 21, position: 'absolute'}} src="https://via.placeholder.com/60x60" />
+        <img style={{width: 60, height: 60, left: 359, top: 21, position: 'absolute'}} src="https://via.placeholder.com/60x60" />
+        <div style={{width: 54, height: 15, left: 19, top: 4, position: 'absolute', color: 'white', fontSize: 15, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>ホーム</div>
+        <div style={{width: 54, height: 15, left: 105, top: 3, position: 'absolute', color: 'white', fontSize: 15, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>駅情報<br/></div>
+        <div style={{width: 65, height: 15, left: 185, top: 2, position: 'absolute', color: 'white', fontSize: 15, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>ポイント</div>
+        <div style={{width: 78, height: 15, left: 350, top: 3, position: 'absolute', color: 'white', fontSize: 15, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>CO2排出量</div>
+        <div style={{width: 49, height: 15, left: 280, top: 3, position: 'absolute', color: 'white', fontSize: 15, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>口コミ</div>
+    </div>
+    <div style={{width: 86, height: 0, left: 83, top: 846, position: 'absolute', transform: 'rotate(90deg)', transformOrigin: '0 0', border: '3px white solid'}}></div>
+    <div style={{width: 430, height: 79, paddingTop: 19, paddingBottom: 20, left: 0, top: 0, position: 'absolute', background: '#5EE030', justifyContent: 'center', alignItems: 'center', display: 'inline-flex'}}>
+        <div style={{width: 107, height: 40, color: 'white', fontSize: 35, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>駅情報</div>
+    </div>
+    <div style={{width: 430, height: 53, left: 0, top: 79, position: 'absolute', background: '#F4F4F3'}}>
+        <div style={{width: 79, height: 0, left: 215, top: 0, position: 'absolute', transform: 'rotate(90deg)', transformOrigin: '0 0', border: '3px #5EE030 solid'}}></div>
+        <div style={{width: 107, height: 40, left: 65, top: 10, position: 'absolute', color: '#69955A', fontSize: 30, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>駅検索</div>
+        <div style={{width: 171, height: 29, left: 240, top: 12, position: 'absolute', color: '#69955A', fontSize: 28, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>駅ランキング</div>
+    </div>
+    <div style={{width: 337, height: 66, left: 52, top: 375, position: 'absolute', background: '#DFDFDE'}}>
+        <img style={{width: 43, height: 43, left: 15, top: 12, position: 'absolute'}} src="https://via.placeholder.com/43x43" />
+        <div style={{width: 171, height: 29, left: 76, top: 19, position: 'absolute', color: '#31312E', fontSize: 28, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>駅を検索</div>
+        <div style={{width: 80, height: 50, left: 247, top: 8, position: 'absolute'}}>
+            <div style={{width: 80, height: 50, left: 0, top: 0, position: 'absolute', background: '#31312E', borderRadius: 10, border: '1px white solid'}} />
+            <div style={{width: 47, height: 34.15, left: 17, top: 11, position: 'absolute', color: 'white', fontSize: 23, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>検索</div>
+        </div>
+    </div>
+    <div style={{width: 337, height: 66, paddingLeft: 111, paddingRight: 111, paddingTop: 17, paddingBottom: 17, left: 53, top: 204, position: 'absolute', background: '#DFDFDE', justifyContent: 'center', alignItems: 'center', display: 'inline-flex'}}>
+        <div style={{width: 115, height: 32, color: '#31312E', fontSize: 28, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>今出川駅</div>
+    </div>
+    <div style={{height: 66, paddingTop: 19, paddingBottom: 18, paddingLeft: 126, paddingRight: 40, left: 52, top: 509, position: 'absolute', background: '#ADE39A', justifyContent: 'flex-end', alignItems: 'center', display: 'inline-flex'}}>
+        <div style={{width: 171, height: 29, color: '#31312E', fontSize: 28, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>烏丸線</div>
+    </div>
+    <div style={{height: 66, paddingTop: 19, paddingBottom: 18, paddingLeft: 126, paddingRight: 40, left: 53, top: 597, position: 'absolute', background: '#EDEEA6', justifyContent: 'flex-end', alignItems: 'center', display: 'inline-flex'}}>
+        <div style={{width: 171, height: 29, color: '#31312E', fontSize: 28, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>東西線</div>
+    </div>
+    <div style={{height: 66, paddingTop: 19, paddingBottom: 18, paddingLeft: 80, paddingRight: 58, left: 52, top: 731, position: 'absolute', background: '#DFDFDE', justifyContent: 'flex-end', alignItems: 'center', display: 'inline-flex'}}>
+        <div style={{width: 199, height: 29, color: '#31312E', fontSize: 28, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>位置情報を開く</div>
+    </div>
+    <div style={{width: 148, height: 29, left: 40, top: 340, position: 'absolute'}}><span style="color: '#69955A', fontSize: 20, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'">● </span><span style="color: 'black', fontSize: 20, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'">駅名で検索</span></div>
+    <div style={{width: 148, height: 29, left: 44, top: 158, position: 'absolute'}}><span style="color: '#69955A', fontSize: 20, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'">● </span><span style="color: 'black', fontSize: 20, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'">最寄駅設定</span></div>
+    <div style={{width: 148, height: 29, left: 39, top: 470, position: 'absolute'}}><span style="color: '#69955A', fontSize: 20, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'">● </span><span style="color: 'black', fontSize: 20, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'">路線で検索</span></div>
+    <div style={{width: 169, height: 29, left: 39, top: 685, position: 'absolute'}}><span style="color: '#69955A', fontSize: 20, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'">● </span><span style="color: 'black', fontSize: 20, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'">現在地から検索</span></div>
+    <div style={{width: 97, height: 41, left: 167, top: 286, position: 'absolute'}}>
+        <div style={{width: 97, height: 41, left: 0, top: 0, position: 'absolute', background: '#69955A', borderRadius: 10, border: '1px white solid'}} />
+        <div style={{width: 50, height: 28, left: 25, top: 6, position: 'absolute', color: 'white', fontSize: 23, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>変更</div>
+    </div>
+</div>
         </div>
     )
 }
