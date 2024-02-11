b5ade76c6b3a8ce1f2f9117c5507e27713e95a5c
Yoshito Ikeda <yoshito.i.2002@gmail.com>
2024-02-11 12:47:45 +0900

    口コミ機能

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,9 @@
 const express = require('express');
 //mysql2を読み込む
 const mysql = require('mysql2');
+
+const session = require('express-session');
+const bcrypt = require('bcrypt');
 //expressモジュールを実体化して、定数appに代入
 const app = express();
 //ポート番号を指定
@@ -14,13 +17,18 @@ const connection = mysql.createConnection({
   database: 'eco_Eki_Net'
 })
 
-//'/'パスにGET要求があった際に実行する処理
-app.get('/', (req, res) => {
-    res.send('Hello world!');
+connection.connect(err => {
+  if (err) {
+    console.error('データベース接続エラー: ', err);
+    return;
+  }
+  console.log('データベースに接続されました...');
 });
 
-//'/api'パスにGET要求があった際に実行する処理
-app.get('/api',(req,res) => {
+app.use(express.json());
+
+//'/'パスにGET要求があった際に実行する処理
+app.get('/', (req, res) => {
   connection.query(
     'select* from test',
     function(err,results,fields){
@@ -31,6 +39,119 @@ app.get('/api',(req,res) => {
       res.json({message: results[0].name});
     }
   )
+});
+
+app.get('/reviews', (req, res) => {
+  const sqlQuery = 'SELECT * FROM reviews';
+  connection.query(sqlQuery, (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
+});
+
+// 口コミ投稿
+app.get('write',(req,res) => {
+  const sqlQuery = 'SELECT* FROM stationName';
+  connection.query(sqlQuery,(err,result) => {
+    if(err) throw err;
+    res.send(result);
+  });
+});
+
+app.post('/reviews', (req, res) => {
+  const newReview = { content: req.body.content};
+  const sqlQuery = 'INSERT INTO reviews SET ?';
+  connection.query(sqlQuery, newReview, (err, result) => {
+    if (err) throw err;
+    res.send('口コミが投稿されました');
+  });
+});
+
+
+//'/api'パスにGET要求があった際に実行する処理
+app.post('/location', (req, res) => {
+  const { latitude, longitude } = req.body;
+  console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
+  // ここで位置情報に基づいた処理を行う
+
+  res.json({message: '位置情報を受け取りました'});
+});
+
+app.post('/signup', 
+  (req, res, next) => {
+    console.log('入力値の空チェック');
+    const username = req.body.username;
+    const password = req.body.password;
+    const errors = [];
+
+    if (username === '') {
+      errors.push('ユーザー名が空です');
+    }
+
+    if (password === '') {
+      errors.push('パスワードが空です');
+    }
+
+    if (errors.length > 0) {
+      res.render('signup.ejs', { errors: errors });
+    } else {
+      next();
+    }
+  },
+  
+  (req, res) => {
+    console.log('ユーザー登録');
+    const username = req.body.username;
+    const password = req.body.password;
+    bcrypt.hash(password, 10, (error, hash) => {
+      connection.query(
+        'INSERT INTO users (username, password) VALUES (?, ?)',
+        [username, hash],
+        (error, results) => {
+          req.session.userId = results.insertId;
+          req.session.username = username;
+          res.redirect('/list');
+        }
+      );
+    });
+  }
+);
+
+app.post('/login', (req, res) => {
+  const email = req.body.email;
+  connection.query(
+    'SELECT * FROM users WHERE email = ?',
+    [email],
+    (error, results) => {
+      if (results.length > 0) {
+        // 定数plainを定義してください
+        const plain = req.body.password;
+        
+        
+        // 定数hashを定義してください
+        const hash = results[0].password
+        
+        // パスワードを比較するためのcompareメソッドを追加してください
+        bcrypt.compare(plain,hash,(error,isEqual) => {
+          if(isEqual){
+            req.session.userId = results[0].id;
+            req.session.username = results[0].username;
+            res.redirect('/list');
+          }else{
+            res.redirect('/login');
+          }
+        })
+      } else {
+        res.redirect('/login');
+      }
+    }
+  );
+});
+
+app.get('/logout', (req, res) => {
+  req.session.destroy((error) => {
+    res.redirect('/list');
+  });
 });
 
 //3001ポートでlisten
